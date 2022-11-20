dad233017a648dc36adf8877e42d86c85357b460
Adam Górka <adamgorka@student.agh.edu.pl>
2022-11-20 17:07:04 +0100

    Lab7 - działa bez GUI

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -1,6 +1,9 @@
 package agh.ics.oop;
+import agh.ics.oop.gui.App;
+import javafx.application.Application;
 public class World {
     public static void main(String[] args) {
+        Application.launch(App.class, args);
         try {
             MoveDirection[] directions = new OptionsParser().parse(args);
             IWorldMap map = new GrassField(10);
--- oolab/src/main/java/agh/ics/oop/gui/App.java
+++ oolab/src/main/java/agh/ics/oop/gui/App.java
@@ -1,10 +1,20 @@
 package agh.ics.oop.gui;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+
 
 public class App extends Application {
+    private GridPane mapGrid;
+
     public void start(Stage primaryStage) {
+        Label label = new Label("Zwierzak");
+        Scene scene = new Scene(label, 400, 400);
+
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 }
